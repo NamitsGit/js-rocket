0bc1e311899907525e127805c558c44954641d29
NamitsGit <career.namitgouranna@gmail.com>
2023-10-08 23:10:03 +0530

    added loops

---
+++ basic-2/loops.js
@@ -0,0 +1,21 @@
+// Program to add first n natural numbers
+/*
+let sum = 0;
+let n = 10;
+for(let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log("The sum of the numbers is  till ", n , " is", sum)
+
+*/
+const a = {
+    namit : 15,
+    neha : 17,
+    omkar : 12,
+    darshan : 14,
+    adi : 14,
+}
+
+for (let i in a) {
+    console.log("The value of ",i," is ",a[i])
+}
---
+++ basic-2/while_loops.js
@@ -0,0 +1,17 @@
+// While
+/*
+let n = 15;
+let i = 0;
+while(i < n) {
+    console.log(i+1)
+    i++
+}
+*/
+
+// Do while
+let i = 0;
+do {
+    console.log(i+1)
+    i++
+} while(i < 15)
+
